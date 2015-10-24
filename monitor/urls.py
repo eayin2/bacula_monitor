d5e31386913ee9626700e775799167347e92e787
eayin2 <>
2015-10-24 20:47:55 +0200

    urls regex for top directory

--- monitor/urls.py
+++ monitor/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url, include
 from monitor import views
 urlpatterns = patterns('',
+    url(r'^$', views.monitor, name='monitor'),
     url(r'^monitor/', views.monitor, name='monitor'),
 )
