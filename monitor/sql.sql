d91672c795191f8d6ba44581d0a6531819413acd
eayin2 <>
2015-10-25 07:32:47 +0100

    borders for sections

--- monitor/sql.sql
+++ monitor/sql.sql
@@ -1,3 +1,4 @@
+## Correct one:
 SELECT c.name, p.name, j.jobbytes, j.realendtime, j.starttime, j.jobfiles 
                      FROM client c 
                      LEFT JOIN LATERAL ( 
@@ -7,3 +8,13 @@ SELECT c.name, p.name, j.jobbytes, j.realendtime, j.starttime, j.jobfiles
                        ORDER BY j.poolid, j.realendtime DESC 
                      ) j ON TRUE 
                      LEFT JOIN pool p ON p.poolid = j.poolid;
+
+
+
+## Another attempt probably wrong:
+SELECT DISTINCT ON (j.clientid, j.poolid) c.name, p.name, j.jobbytes, j.realendtime, j.starttime, j.jobfiles
+  FROM job j
+  LEFT JOIN client c  ON c.clientid = j.clientid
+  LEFT JOIN pool p  ON p.poolid = j.poolid
+  WHERE j.jobstatus IN ('T', 'W') AND j.level IN ('F', 'I', 'D') AND j.type IN ('B', 'C')
+  ORDER BY j.clientid, j.poolid, j.realendtime DESC
